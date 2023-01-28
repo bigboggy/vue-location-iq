a89f4d68fa2c7a715313cca2d2a2b943430e81c2
bigboggy <bogdan@meshnet.capital>
2023-01-28 12:47:27 -0800

    chore: update vite config

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,12 @@
-import "path";
+import { resolve } from "path";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import * as path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
     lib: {
-      entry: path.resolve(__dirname, "src/components/index.js"),
+      entry: resolve(__dirname, "src/components/index.js"),
       name: "VueLocationIQ",
       fileName: (format) => `vue-location-iq.${format}.js`,
     },
